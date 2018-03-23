82b3d3b17d1b6cb276a0339afcb222241b62dd5c
ext-yuren.zhang <ext-yuren.zhang@le.com>
2018-03-23 11:45:36 +0800

    pc7

--- s.sh
+++ s.sh
@@ -1,11 +1,8 @@
-#!/bin/bash
-read -p"input you name:" name
-echo "hello! $name let go game!"
-echo "=======是否copy最新测试资源 Y or N======"
-read chose
-echo $chose
-if [ $chose == "Y" ] || [ $chose == "y" ];then
-echo "chose correct"
-else
-echo "chose error"
-fi
+#!/bin/bash -
+#a=5
+#[ ${a} -eq 5 ]&& echo "number is eq" ||echo "number is no"
+read -p "input you world:" number
+cat wub.txt|grep -a -x -A 1 "${number}"|sed -n 2'p'
+	
+
+
